6d595cf83efaf35d159ccd54b2e066a94d5b9de0
AshkanArabim <ashkan.arabim@gmail.com>
2023-01-25 06:43:46 -0700

    add node factory

---
+++ node.js
@@ -0,0 +1,23 @@
+// quite an interesting file name
+
+export default function nodeFactory (data) {
+  let leftChild , rightChild;
+  const getData = () => {
+    return this.data();
+  }
+  const setData = (newVal) => {
+    this.data = newVal;
+  }
+  const getLeftChild = () => {
+    return this.leftChild;
+  }
+  const getRightChild = () => {
+    return this.rightChild
+  }
+  const setLeftChild = (child) => {
+    this.leftChild = child;
+  }
+  const setRightChild = (child) => {
+    this.rightChild = child;
+  }
+}
